7e2083db951379c0f7209f27443bdfbc30d564ea
Mike Sinkovsky <msink@users.noreply.github.com>
2016-11-21 17:49:30 +0500

    Update appveyor.bat

--- appveyor.bat
+++ appveyor.bat
@@ -7,14 +7,19 @@ mkdir %build%
 
 if %compiler%==mingw (
   set "PATH=C:\msys64\mingw%bit%\bin;%PATH%"
-  meson %build%
+  meson %build% --backend ninja
   ninja -C %build%
   file %build%\hello
   %build%\hello
 
 ) else if %compiler%==msvc (
-  if %bit%==32 (call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x86)
-  if %bit%==64 (call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" amd64)
+  if %bit%==32 (
+    call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x86
+    echo x86
+  ) else if %bit%==64 (
+    call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" amd64
+    echo amd64
+  )
   meson %build% --backend vs2015
   MSBuild %build%\hello.sln
   file %build%\hello
