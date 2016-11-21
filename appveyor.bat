44465716db7682cc5fac0045006be56546ebdb26
Mike Sinkovsky <msink@users.noreply.github.com>
2016-11-21 18:57:46 +0500

    Update appveyor.bat

--- appveyor.bat
+++ appveyor.bat
@@ -1,5 +1,7 @@
+echo
 echo Compiler: %compiler%
 echo Bits: %bit%
+echo
 
 set "PATH=%PATH%;C:\msys64\usr\bin"
 set build=build\%compiler%-%bit%
@@ -9,22 +11,17 @@ if %compiler%==mingw (
   set "PATH=C:\msys64\mingw%bit%\bin;%PATH%"
   meson %build% --backend ninja
   ninja -C %build%
-  file %build%\hello
-  %build%\hello
 
-) else if %compiler%==msvc (
-  if %bit%==32 (
-    call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" amd64_x86
-    meson %build% --backend vs2015
-    type %build%\hello.sln
-    MSBuild %build%\hello.sln /p:Configuration=release /p:Platform=Win32
-  ) else if %bit%==64 (
-    call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" amd64
-    meson %build% --backend vs2015
-    type %build%\hello.sln
-    MSBuild %build%\hello.sln /p:Configuration=release /p:Platform=x64
-  )
-  file %build%\hello
-  %build%\hello
+) else if %compiler%%bit%==msvc32 (
+  call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" amd64_x86
+  meson %build% --backend vs2015
+  MSBuild %build%\hello.sln /p:Configuration=release /p:Platform=Win32
 
+) else if %compiler%%bit%==msvc64 (
+  call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" amd64
+  meson %build% --backend vs2015
+  MSBuild %build%\hello.sln /p:Configuration=release /p:Platform=x64
 )
+
+file %build%\hello
+%build%\hello
