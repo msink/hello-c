d951aaf719291c96d2e93090784b95026c085597
Mike Sinkovsky <msink@permonline.ru>
2016-11-26 14:39:52 +0500

    reconfigure

--- appveyor.bat
+++ appveyor.bat
@@ -8,26 +8,37 @@ echo.
 
 set "PATH=%PATH%;C:\msys64\usr\bin"
 set build=build\%Compiler%-%Platform%-%Configuration%
-mkdir %build%
 
 if %Compiler%-%Platform%==mingw-x86 (
   set "PATH=C:\msys64\mingw32\bin;%PATH%"
-  meson %build% --backend ninja --buildtype %Configuration%
+  if not exist %build% (
+    mkdir %build%
+    meson %build% --backend ninja --buildtype %Configuration%
+  )
   ninja -C %build%
 
 ) else if %Compiler%-%Platform%==mingw-x64 (
   set "PATH=C:\msys64\mingw64\bin;%PATH%"
-  meson %build% --backend ninja --buildtype %Configuration%
+  if not exist %build% (
+    mkdir %build%
+    meson %build% --backend ninja --buildtype %Configuration%
+  )
   ninja -C %build%
 
 ) else if %Compiler%-%Platform%==msvc-x86 (
   call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" amd64_x86
-  meson %build% --backend vs2015 --buildtype %Configuration%
+  if not exist %build% (
+    mkdir %build%
+    meson %build% --backend vs2015 --buildtype %Configuration%
+  )
   MSBuild %build%\hello.sln /p:Platform=Win32
 
 ) else if %Compiler%-%Platform%==msvc-x64 (
   call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" amd64
-  meson %build% --backend vs2015 --buildtype %Configuration%
+  if not exist %build% (
+    mkdir %build%
+    meson %build% --backend vs2015 --buildtype %Configuration%
+  )
   MSBuild %build%\hello.sln
 )
 
