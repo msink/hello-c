d474b13731d340f4319644bcbe7817e135254a12
Mike Sinkovsky <msink@permonline.ru>
2016-12-06 13:40:23 +0500

    formatting

--- appveyor.bat
+++ appveyor.bat
@@ -21,21 +21,21 @@ if %compiler:windows-=%==%compiler% (
 
 if not exist %build% (
   mkdir %build%
-  if not %compiler:linux-=%==%compiler% (
+  if not %compiler%==%compiler:linux-=% (
     meson %build% --buildtype %configuration% ^
 		--cross-file compilers\%compiler%-%platform%.txt || exit /b
-  ) else if not %compiler:osx-=%==%compiler% (
+  ) else if not %compiler%==%compiler:osx-=% (
     meson %build% --buildtype %configuration% ^
 		--cross-file compilers\%compiler%-%platform%.txt ^
 		-D b_lundef=false -D b_asneeded=false || exit /b
-  ) else if not %compiler:-msbuild=%==%compiler% (
+  ) else if not %compiler%==%compiler:-msbuild=% (
     meson %build% --buildtype %configuration% --backend vs2015 || exit /b
   ) else (
     meson %build% --buildtype %configuration% || exit /b
   )
 )
 
-if %compiler:-msbuild=%==%compiler% (
+if %compiler%==%compiler:-msbuild=% (
   ninja -C %build% || exit /b
 ) else if %platform%==X86 (
   MSBuild %build%\hello.sln /verbosity:minimal /p:Platform=Win32 || exit /b
@@ -48,15 +48,15 @@ if %compiler%-%configuration%==windows-mingw-release (
 )
 
 echo.
-if not %compiler:windows-=%==%compiler% (
+if not %compiler%==%compiler:windows-=% (
   C:\msys64\usr\bin\file  %build%\hello.exe
   C:\msys64\usr\bin\wc -c %build%\hello.exe
   C:\msys64\usr\bin\size  %build%\hello.exe
   %build%\hello
-) else if not %compiler:osx-=%==%compiler% (
+) else if not %compiler%==%compiler:osx-=% (
   C:\msys64\usr\bin\file  %build%\hello
   C:\msys64\usr\bin\wc -c %build%\hello
-  rem C:\cygwin64\bin\x86_64-apple-darwin14-size %build%\hello
+  C:\cygwin-cross\bin\x86_64-apple-darwin14-size %build%\hello
 ) else (
   C:\msys64\usr\bin\file  %build%\hello
   C:\msys64\usr\bin\wc -c %build%\hello
